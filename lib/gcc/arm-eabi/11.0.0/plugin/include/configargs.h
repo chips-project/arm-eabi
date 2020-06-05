295e64d91021634c1f738cd38807efd2e544cf78
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-06 03:09:52 +0700

    arm-eabi-gcc (GCC) 11.0.0 20200605 (experimental)
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

---
+++ lib/gcc/arm-eabi/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/najahi/workspace/.build/arm-eabi/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=arm-eabi --prefix=/home/najahi/x-tools/arm-eabi --with-local-prefix=/home/najahi/x-tools/arm-eabi/arm-eabi --with-headers=/home/najahi/x-tools/arm-eabi/arm-eabi/include --with-newlib --enable-threads=no --disable-shared --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/najahi/workspace/.build/arm-eabi/buildtools --with-mpfr=/home/najahi/workspace/.build/arm-eabi/buildtools --with-mpc=/home/najahi/workspace/.build/arm-eabi/buildtools --with-isl=/home/najahi/workspace/.build/arm-eabi/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
